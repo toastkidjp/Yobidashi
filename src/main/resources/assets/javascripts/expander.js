90037af5503defb4161644853a08ae30eccd41ba
Toast kid <super_q1m@yahoo.co.jp>
2017-01-10 19:44:57 +0900

    implement smooth scroll in page link.

--- src/main/resources/assets/javascripts/expander.js
+++ src/main/resources/assets/javascripts/expander.js
@@ -4,14 +4,30 @@
  */
 var isOpen = false;
 function open(expanderID){
-	if (!isOpen) {
-		document.getElementById(expanderID).style.display = "block";
-		isOpen = true;
-	} else {
-		close(expanderID);
-	}
+  if (!isOpen) {
+    document.getElementById(expanderID).style.display = "block";
+    isOpen = true;
+  } else {
+    close(expanderID);
+  }
 }
+
 function close(expanderID){
-	document.getElementById(expanderID).style.display = "none";
-	isOpen = false;
+  document.getElementById(expanderID).style.display = "none";
+  isOpen = false;
 }
+
+/**
+ *
+ * <a href ="http://kyasper.com/jquery-tips/">
+ * jQuery とっても簡単、ページ内リンクでスムーズスクロール</a>
+ */
+$(function(){
+    $('a[href^=#]').click(function() {
+       var href= $(this).attr("href");
+       var target = $(href == "#" || href == "" ? 'html' : href);
+       var position = target.offset().top;
+       $('body,html').animate({scrollTop:position}, 300, 'swing');
+       return false;
+    });
+});
